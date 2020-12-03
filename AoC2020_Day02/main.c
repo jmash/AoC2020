a606885eb40770ae644edff100786eadafedcc44
Jared Ashcraft <jaredmashcraft@gmail.com>
2020-12-02 23:05:31 -0500

    fool me twice, shame on me

--- AoC2020_Day02/main.c
+++ AoC2020_Day02/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char *argv[])
+typedef struct
 {
-  printf("Hello World\n");
-  return 0;
+    int min;
+    int max;
+    char checkChar;
+    char * password;
+} password_check_t;
+
+void resetPasswordCheck(password_check_t * checkLine)
+{
+    checkLine->min = 0;
+    checkLine->max = 0;
+    checkLine->checkChar = 'a';
+    checkLine->password = "abc";
+}
+
+int main()
+{
+    FILE input = fopen("./testinput.txt", "r");
+    char currCh;
+    currCh = fgetc(input);
+    check_password_t checkLine;
+    check_password_t * checkLinePtr = checkLine;
+    int passwordCharCount = 0;
+
+
+    resetPasswordCheck(checkLinePtr);
+
+    ungetc(currCh, input);
+
+    while(currCh != EOF)
+    {
+        if(currCh == '\n')
+        {
+            passwordCharCount = 0;
+        }
+        printf("%c", currCh);
+        currCh = fgetc(input);
+    }
+
+    return 0;
 }
