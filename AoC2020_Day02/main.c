d33e21d56bbab313e2fa766e5d5d1bec5d47436c
Jared Ashcraft <jaredmashcraft@gmail.com>
2020-12-02 23:58:34 -0500

    First part done! :D

--- AoC2020_Day02/main.c
+++ AoC2020_Day02/main.c
@@ -19,15 +19,15 @@ void resetPasswordCheck(check_password_t * checkLine)
 
 int main()
 {
-    FILE * input = fopen("./testinput.txt", "r");
+    FILE * input = fopen("./input.txt", "r");
     char currCh = fgetc(input);
     check_password_t checkLine;
     check_password_t * checkLinePtr = &checkLine;
     int passwordCharCount = 0;
+    int validPasswordCount = 0;
 
     checkLine.password = malloc(4*sizeof(char));
     strcpy(checkLine.password, "abc");
-    printf("%s\n", checkLine.password);
 
     resetPasswordCheck(checkLinePtr);
 
@@ -37,14 +37,35 @@ int main()
     {
         if(currCh == '\n')
         {
+            char * passwordChPtr = checkLine.password;
+            int checkCharCount = 0; 
+            while(*(passwordChPtr) != '\0')
+            {
+                if(*(passwordChPtr) == checkLine.checkChar)
+                {
+                    checkCharCount++;
+                }
+                passwordChPtr++;
+            }
+            if(checkCharCount >= checkLine.min && checkCharCount <= checkLine.max)
+            {
+                validPasswordCount++;
+            }
             passwordCharCount = 0;
             resetPasswordCheck(checkLinePtr);
         }
 
-
-        fscanf(input, "%d-%d %c: ", &checkLine.min, &checkLine.max, &checkLine.checkChar);
+        if(currCh == ':')
+        {
+            checkLine.password = realloc(checkLine.password, passwordCharCount*sizeof(char)+1); 
+        }
+            
+        fscanf(input, "%d-%d %c: %s", &checkLine.min, &checkLine.max, &checkLine.checkChar, checkLine.password);
+        passwordCharCount++;
         currCh = fgetc(input);
     }
+    printf("%d\n", validPasswordCount);
 
+    free(checkLine.password);
     return 0;
 }
