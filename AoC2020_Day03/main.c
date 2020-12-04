41985bfda557532735140c2281a49bbe171b9a80
Jared Ashcraft <jaredmashcraft@gmail.com>
2020-12-04 01:58:42 -0500

    'bout to wreck it

--- AoC2020_Day03/main.c
+++ AoC2020_Day03/main.c
@@ -1,13 +1,53 @@
 #include <stdio.h>
 
+// Output should be: .#.##.####
+
 int main(int argc, const char *argv[])
 {
     FILE * input = fopen("./testinput.txt", "r");
-    char currCh;
+    char currCh = fgetc(input);
+    int charIter = 0;
+    int colPos = 0;
+    int numCols = 0;
+    int treeCount = 0;
+    int i = 0;
 
+    while((currCh = fgetc(input)) != '\n')
+    {
+        numCols++;
+    }
+    numCols++;
+    printf("columns %d\n", numCols);
+    fseek(input, 0, SEEK_SET);
+
+    fgetc(input);
+
+    printf("O");
     while((currCh = fgetc(input)) != EOF)
     {
-        printf("%c");
+        ++charIter;
+        if(colPos > numCols)
+        {
+            charIter = 0;
+            colPos = 0;
+        }
+        if(currCh == '\n') 
+        {
+            colPos += 3;
+            --charIter;
+        }
+        if(charIter % (numCols+3) == 0) 
+        {
+            if(currCh == '.') printf("O");
+            if(currCh == '#') 
+            {
+                treeCount++;
+                printf("X");
+            }
+            if(currCh == '\n') printf("\n");
+        } else
+        printf("%c", currCh);
     }
+    printf("Tree count: %d\n", treeCount);
     return 0;
 }
