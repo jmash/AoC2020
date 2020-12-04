49be0bd68b7b1a6f76d7767d3cb0c22b010dc93c
Jared Ashcraft <jaredmashcraft@gmail.com>
2020-12-04 11:25:14 -0500

    Day 4 init setup

--- AoC2020_Day04/main.c
+++ AoC2020_Day04/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+typedef struct
+{
+    char * key;
+    char * value;
+} passport_field_t;
+
+typedef struct
+{
+    passport_field_t ecl;
+    passport_field_t pid;
+    passport_field_t eyr;
+    passport_field_t hcl;
+    passport_field_t byr;
+    passport_field_t iyr;
+    passport_field_t cid;
+    passport_field_t hgt;
+} passport_t;
+
 int main(int argc, const char *argv[])
 {
-  printf("Hello World\n");
-  return 0;
+    FILE * input = fopen("./testinput.txt", "r");
+    
+    return 0;
 }
