1a8784e87cc14450a5456f88de35d2dac68360d1
Jared Ashcraft <jaredmashcraft@gmail.com>
2020-12-06 15:58:35 -0500

    Starting strong! :)

--- AoC2020_Day06/main.c
+++ AoC2020_Day06/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_ALPHA 26
+
+void printArray(bool arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
 
 int main(int argc, const char *argv[])
 {
-  printf("Hello World\n");
-  return 0;
+    bool groupAnswers[MAX_ALPHA];
+    memset(groupAnswers, 0, MAX_ALPHA*sizeof(bool));
+
+    printArray(groupAnswers, MAX_ALPHA);
+
+    return 0;
 }
