71a352f49cf7f67c19c33ff5b41072007f983af7
Jared Ashcraft <jaredmashcraft@gmail.com>
2020-12-12 14:37:25 -0500

    started Day 9! Exciting! :D

--- AoC2020_Day09/main.c
+++ AoC2020_Day09/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/*
+   [35   35   35
+    20  [20   20
+    15   15  [15
+    25   25   25
+    47]  47   47
+    (40) 40]  40
+    62   (62) 62]
+    55   ...  (55)
+    65        ...
+    95
+    102
+    117
+    150
+    182
+    127
+    219
+    299
+    277
+    309
+    576 
+*/
+
+int readLines(FILE * input)
+{
+    int numLines = 0;
+    char currCh; // hi Currch :)
+    while((currCh = fgetc(input)) != EOF)
+    {
+        if(currCh == '\n') numLines++;
+    }
+    return numLines;
+}
 
 int main(int argc, const char *argv[])
 {
-  printf("Hello World\n");
-  return 0;
+    FILE * input = fopen("./testinput.txt", "r");
+    int fileLines = readLines(input); 
+
+    printf("%d\n", fileLines);
+    return 0;
 }
